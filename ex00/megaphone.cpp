11e2da122b35688c6b5959ff5da908a0d73cc878
Amauri Lisboa <amauri_lisboa@Air-de-Amauri.fritz.box>
2024-10-23 18:22:28 +0200

    Exercise 00 done with ternary

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
+int	megaphone_off(void)
+{
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (EXIT_FAILURE);
+}
+
+int	megaphone_on(char argc, char **argv)
+{
+	for(int i = 1; i < argc; i ++)
+	{
+		for (int j = 0; argv[i][j]; j ++)
+			std::cout << (char)std::toupper(argv[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE*" << std::endl;
-        return (EXIT_FAILURE);
-    }
-    for(int i = 1; i < argc; i ++)
-        for (int j = 0; argv[i][j]; j ++)
-            std::cout << (char)std::toupper(argv[i][j]);
-    std::cout << std::endl;
-    return (0);
+	return (argc < 2 ? megaphone_off() : megaphone_on(argc, argv));
 }
