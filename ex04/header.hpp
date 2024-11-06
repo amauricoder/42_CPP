58effe7129b7a2cf78b2580b9f5c19c6c67ff8db
Amauri Lisboa <amauri.olisboa@gmail.com>
2024-11-06 15:16:13 +0000

    Exercise04 done

--- ex04/header.hpp
+++ ex04/header.hpp
@@ -6,7 +6,7 @@
 /*   By: aconceic <aconceic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:03:22 by aconceic          #+#    #+#             */
-/*   Updated: 2024/11/05 17:51:43 by aconceic         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:48:29 by aconceic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 #include <iostream>
 #include <fstream> //::ifstream
+#include <cstring> //c_str()
 
-int argument_error(int argc, char **argv);
-int	get_qt_lines(std::ifstream &file);
+int			err(std::string msg, int exit);
+int 		argument_error(int argc, char **argv);
+int			get_qt_lines(std::ifstream &file);
+std::string replace_word(char **argv, int qt_lines, std::ifstream &file);
 
 #endif
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,34 +6,33 @@
 /*   By: aconceic <aconceic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 16:01:01 by aconceic          #+#    #+#             */
-/*   Updated: 2024/11/05 18:14:07 by aconceic         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:15:31 by aconceic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
+/**
+ * @brief Exercise CPP01 ex04 - Read subject for requirements.
+ * Check arguments validity, open the file, replace s1 for s2 and save it
+ * in a string, create the new file and save the string in the new_file.
+ */
 int main(int argc, char **argv)
 {
 	if (argument_error(argc, argv))
 		return (1);
 	
 	std::ifstream	file(argv[1]);
-	std::string		s1(argv[2]);
-	std::string		s2(argv[3]);
 	int				qt_lines = get_qt_lines(file);
-	std::string		arr[qt_lines];
 
 	file.clear();          // Clear any EOF or fail flags
     file.seekg(0);         // Move back to the beginning of the file
-
-	//Minha ideia aqui eh ler a linha com get_line(), entao
-	//verificar se na linha tem a palavra s1, se tiver, preciso substituila por s2.
-	// You can use a combination of find(),
-	//erase(), and insert() to locate and replace parts of a string.
-	for (int i = 0; i < qt_lines; i ++)
-	{
-		getline(file, arr[i]);
-		std::cout << "LINE CONTENT -> " << arr[i] << std::endl;
-	} 	
+	std::string		lines_replaced = replace_word(argv, qt_lines, file);;
+	std::string		new_file_name = std::string(argv[1]) + ".replace";
+	std::ofstream	new_file;
+	new_file.open(new_file_name.c_str());
+	if (new_file.fail())
+		return (err("Problem creating the file", 1));
+	new_file << lines_replaced;
 	return (0);
 }
