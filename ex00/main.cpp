035b7675763b7945ca32466e2a84d55a15dbdaa7
Amauri Lisboa <amauri.olisboa@gmail.com>
2024-12-09 18:52:37 +0000

    starting CPP05

---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aconceic <aconceic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 14:51:57 by aconceic          #+#    #+#             */
+/*   Updated: 2024/12/09 16:10:40 by aconceic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "tests.hpp"
+
+int main(void)
+{
+	test_dog_make_sound();
+	test_cat_make_sound();
+	test_animal_make_sound();
+	test_inherit_dog_make_sound();
+	test_inherit_cat_make_sound();
+	test_subject();
+	return (0);
+}
